c30393982d7d2d9e36141eb4b116b281ccc935d0
benja <ruizbr2020@gmail.com>
2021-06-18 21:35:03 +0200

    final-update

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,28 +1,36 @@
 from textblob import TextBlob
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from tkinter import IntVar, Menu, Tk
 
 import nltk
 import csv
 
+"""sentiment_analyzer.py: Un script que analiza los comentarios del Ice-hotel 
+                          les asigna un score segun polaridad,sentimiento y realiza un estudio NER
+                          y extrae las entidades que reconoce """
 
-text = ''' ",You can visit the hotel and be wowed by the artistry of the rooms, but if you come, you have to spend the night in a cold room. This is what this place is all about! The beauty of the rooms is secondary to the experience of spending the night in one. The anticipation, fear, excitement... and when all is done you will have an experience that you will remember forever. We stayed in the ice hotel for 2 nights. The first in a hot room and the second in a cold one. The hot room was very nice and clean. For the cold one we went all out and stayed in a cold suite with a warm bathroom. First let me explain a little bit. When you stay overnight in a cold room, the room is made of ice with only a bed and nothing else. If you need to use the bathroom, you have to walk to an adjacent"
-", building that is warm. The building is a service center with coffee, showers, lockers, sauna and is staffed 24/7. You have access to your room after 6 PM. When you go to sleep, you leave your stuff in the service center and only take your thermal underwear, a cap and your sleeping bag. The sleeping bag is warm and it covers your head. You are also given a liner to use inside of the sleeping bag. I found the bed and sleeping bag to be confortable and once I overcame the excitement, I was able to get an excellent night sleep. The rooms are very dark. They wake you up in the morning. Our room was a suite so we had a hot bathroom with showers and we had our luggage with us. If you can afford it, this makes the stay very pleasant and does not change the experience of sleeping in the ice room. In my opinion, if you don’t stay in a suite with the warm bathroom, there is no point in staying on a art room or a basic ice room. Both are beautiful and at night you just sleep. During the day, you will have time to visit all the rooms and take pictures of them, you will not miss a thing. After finishing my Northern Lights trip, I would not have stayed at the Ice Hotel for two nights. There are a lot of activities, but I would have rather stayed at the Abisko Tourist Station an extra night to enjoy better scenery and similar activities in a less touristy environment. Don’t hesitate to spend a night here. It will be an experience to remember"
-",…" '''
+__author__      = "Benjamin Ruiz"
+__copyright__   = "GPLv3"
 
-blob = TextBlob(text)
-worf_quote = "Sir, I protest. I am not a merry man!"
 
-words_in_quote = word_tokenize(worf_quote)
+chuck_quote = '''Chuck Norris died and came and he decided to come back because after life was boring him'''
 
-print(words_in_quote)
-
+# Tokenizo la frase celebre de Chuck Norris
+words_in_quote = word_tokenize(chuck_quote)
+# Consigo una lista de "stop-words" del lenguaje en este caso ingles
 stop_words = set(stopwords.words("english"))
+# Ignoro las "stop-words" y consigo una frase sin ellas 
+lista_filtrada = [palabra for palabra in words_in_quote if palabra.casefold() not in stop_words]
+print(f"\nFrase orgininal --> {chuck_quote}")
+print(f"Frase Tokenizada --> {words_in_quote}")
+print(f"Frase sin stop-words --> {lista_filtrada}")
+tags = nltk.pos_tag(words_in_quote)
+print(tags)
+tree = nltk.ne_chunk(tags)
+tree.draw()
 
-filterd_list = [word for word in words_in_quote if word.casefold() not in stop_words]
-print(filterd_list)
-
-def extract_ne(quote):
+def extraer_entidades(quote):
      words = word_tokenize(quote, language="english")
      # POS Parts Of Speach tagging se emplea para poder "leer el texto" 
      # entendiendo que rol gramatical que tiene cada palabra segun su contexto
@@ -64,28 +72,28 @@ def extract_ne(quote):
                     "money": money, "percent": percent,"facility": facility,
                     "gpe": gpe}
      
+     print("Entidades-----------------")
      for key,value in namedEntities.items():
           if value == set():
                value = "no encontrado"
 
-          print(key ,":", value )       
-     
-     return namedEntities
+          print(" >",key ,":", value )
+
 
       
 def extraer_sentimiento():
- with open('IceHotelcomments.csv', 'r', encoding="utf8") as file:
+ with open('IceHotelComments.csv', 'r', encoding="utf8") as file:
      reader = csv.reader(file,delimiter ='"')
      x=1
      for row in reader:
-         print('Comentario nº %d'%(x))   
+         print('\n\n______Comentario nº %d______'%(x))   
          print(row)
          comment=TextBlob(str(row))
-         print(format(comment.sentiment))
-         print('\n\n')
+         print("Analisis sentimiento -----")
+         print(" > Polaridad = {0:.4f}".format(comment.sentiment[0]))
+         print(" > Sentimiento = {0:.4f}".format(comment.sentiment[1]))
+         extraer_entidades(str(comment))
          x+=1
         
-
-#print (extract_ne(text))
 
 extraer_sentimiento()
